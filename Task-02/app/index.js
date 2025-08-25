a2cc05c0f4b4d0a897a5848ee3ce0f82a3718796
drxcy <rachitbahuguna5@gmail.com>
2025-08-25 14:08:48 +0530

    Updation in index.js so it can appears in github pages coz it doesnt serve node.js directly

--- Task-02/app/index.js
+++ Task-02/app/index.js
@@ -4,7 +4,18 @@ const PORT = 3000;
 
 app.get('/', (req,res) =>
 {
-    res.send("<h1> Hello Welcome to the Elite Inter </h1> <p>This is show to that CI / CD pipelines Successfully deployed </p> ")
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Elite Intern</title>
+    </head>
+    <body style="text-align:center; margin-top:40px;">
+      <h1>Hello, Welcome to the Elite Intern</h1>
+      <p>This shows that CI/CD pipeline was successfully deployed 🎉</p>
+    </body>
+    </html>
+  `);
 })
 app.listen(PORT,() =>{  
 console.log(`Server is running on http://localhost:${PORT}`);
